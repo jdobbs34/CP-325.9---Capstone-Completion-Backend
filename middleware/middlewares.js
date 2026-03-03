1d6aa2df527fe373fff619a569e0aefa43b5abdf
jdobbs34 <dobbsjason34@yahoo.com>
2026-03-03 11:17:14 -0700

    Created middlewares.js

---
+++ middleware/middlewares.js
@@ -0,0 +1,14 @@
+export function loqReq(req, _res, next) {
+  console.log(
+    `${req.method} -- ${req.url} -- ${new Date().toLocaleTimeString()}`,
+  );
+
+  if (req.body) console.table(req.body);
+
+  next();
+}
+
+export function globalErr(err, req, res, next) {
+  res.status(err.status || 500).json({ error: `❌ Error: ${err.message} `})
+}
+
