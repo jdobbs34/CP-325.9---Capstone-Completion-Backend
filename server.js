d8cd796085577e0a46b2d5934b8e588ed56d2414
jdobbs34 <dobbsjason34@yahoo.com>
2026-03-03 11:36:20 -0700

    6th commit

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 // Imports
 import express from "express"
 import dotenv from "dotenv"
-
+import {logReq, globalErr} from "./middleware/middlewares.js"
+import cors from "cors"
 
 // Setups
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+
 // Middleware
 app.use(cors())
 app.use(express.json())
+app.use(logReq)
 
 // Routes
 
 
 // Global Err
+app.use(globalErr)
 
 // Listener
 app.listen(PORT, () => {
