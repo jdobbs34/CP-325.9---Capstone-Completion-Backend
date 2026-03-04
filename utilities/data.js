56f2a72c52c77f3b15f49d201ad1c844d01cc2d2
jdobbs34 <dobbsjason34@yahoo.com>
2026-03-04 09:44:47 -0700

    10th commit

---
+++ utilities/data.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    status: "finished",
+    rating: 5,
+    notes: "A classic. The one that started it all.",
+    cover: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1",
+    googleId: "wrOQLV6xB-wC"
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    status: "finished",
+    rating: 5,
+    notes: "Scary how relevant this still is today.",
+    cover: "http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&zoom=1",
+    googleId: "kotPYEqx7kMC"
+  },
+  {
+    title: "Dune",
+    author: "Frank Herbert",
+    status: "reading",
+    rating: 4,
+    notes: "Slow start but picks up fast. Love the world building.",
+    cover: "http://books.google.com/books/content?id=B5PbAAAAMAAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "B5PbAAAAMAAJ"
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    status: "want to read",
+    rating: null,
+    notes: "",
+    cover: "http://books.google.com/books/content?id=iXn5U2IziiYC&printsec=frontcover&img=1&zoom=1",
+    googleId: "iXn5U2IziiYC"
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    status: "finished",
+    rating: 5,
+    notes: "One of the best books I have ever read.",
+    cover: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "PGR2AwAAQBAJ"
+  },
+  {
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    status: "reading",
+    rating: 3,
+    notes: "Interesting so far. Very philosophical.",
+    cover: "http://books.google.com/books/content?id=FzVjBgAAQBAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "FzVjBgAAQBAJ"
+  }
+]
+
