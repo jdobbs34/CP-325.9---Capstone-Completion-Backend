3e33bc2898037f91cea6f7adca48c66036f90315
jdobbs34 <dobbsjason34@yahoo.com>
2026-03-05 15:32:18 -0700

    Worked on README and added to data to data.js

--- utilities/data.js
+++ utilities/data.js
@@ -52,6 +52,96 @@ export default  [
     notes: "Interesting so far. Very philosophical.",
     cover: "http://books.google.com/books/content?id=FzVjBgAAQBAJ&printsec=frontcover&img=1&zoom=1",
     googleId: "FzVjBgAAQBAJ"
+  },
+  {
+    title: "The Hunger Games",
+    author: "Suzanne Collins",
+    status: "finished",
+    rating: 5,
+    notes: "Could not put it down. Read it in one sitting.",
+    cover: "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "sazytgAACAAJ"
+  },
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    status: "finished",
+    rating: 5,
+    notes: "A perfect adventure story. Better than the movies.",
+    cover: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1",
+    googleId: "pD6arNyKyi8C"
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    status: "reading",
+    rating: 4,
+    notes: "Very practical. Already applying some of the habits.",
+    cover: "http://books.google.com/books/content?id=XfFvDwAAQBAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "XfFvDwAAQBAJ"
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    status: "finished",
+    rating: 3,
+    notes: "Interesting but Holden is hard to like sometimes.",
+    cover: "http://books.google.com/books/content?id=PCDragEACAAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "PCDragEACAAJ"
+  },
+  {
+    title: "Brave New World",
+    author: "Aldous Huxley",
+    status: "want to read",
+    rating: null,
+    notes: "",
+    cover: "http://books.google.com/books/content?id=eSa0AAAAIAAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "eSa0AAAAIAAJ"
+  },
+  {
+    title: "The Midnight Library",
+    author: "Matt Haig",
+    status: "finished",
+    rating: 4,
+    notes: "Emotional and thought provoking. Made me reflect on my own choices.",
+    cover: "http://books.google.com/books/content?id=TNmEDwAAQBAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "TNmEDwAAQBAJ"
+  },
+  {
+    title: "Think and Grow Rich",
+    author: "Napoleon Hill",
+    status: "reading",
+    rating: 4,
+    notes: "Classic self help book. Some parts feel outdated but the core ideas are solid.",
+    cover: "http://books.google.com/books/content?id=Dj3SDAAAQBAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "Dj3SDAAAQBAJ"
+  },
+  {
+    title: "The Power of Now",
+    author: "Eckhart Tolle",
+    status: "want to read",
+    rating: null,
+    notes: "Recommended by a friend. Looking forward to reading it.",
+    cover: "http://books.google.com/books/content?id=4-hDDwAAQBAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "4-hDDwAAQBAJ"
+  },
+  {
+    title: "Fahrenheit 451",
+    author: "Ray Bradbury",
+    status: "finished",
+    rating: 5,
+    notes: "Scary how relevant this is today with social media and short attention spans.",
+    cover: "http://books.google.com/books/content?id=tXopCgAAQBAJ&printsec=frontcover&img=1&zoom=1",
+    googleId: "tXopCgAAQBAJ"
+  },
+  {
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    status: "finished",
+    rating: 4,
+    notes: "Changed how I think about money. Everyone should read this.",
+    cover: "http://books.google.com/books/content?id=4-hDZtpTF8YC&printsec=frontcover&img=1&zoom=1",
+    googleId: "4-hDZtpTF8YC"
   }
 ]
 
